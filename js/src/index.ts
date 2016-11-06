3b0b594bc725f12c65f12442845bcd1eee21a8e9
Andrii Maglovanyi <andrii.maglovanyi@gmail.com>
2016-11-06 11:04:13 +0200

    Compiled project

--- js/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { Chessboard } from "./Chessboard";
-import { Knight } from "./Knight";
-import { PathFinder } from "./PathFinder";
-
-var chessboard = new Chessboard(document.getElementById("board"), [8, 8]);
-new PathFinder(chessboard.matrix, chessboard.table, new Knight());
